efdea040ff7382056c0014f4340c8bb93590f934
jczetta <jczetta@gmail.com>
2017-11-14 15:26:12 -0500

    Initial commit code files

---
+++ README.md
@@ -0,0 +1,53 @@
+# SI 364: Homework 5
+
+### Deadline: Monday, November 27 at 11:59 PM
+
+## To Submit
+
+Fork and clone this repository. Make edits to it as appropriate (see below), add, commit, and push them to your fork.
+
+* **For Part 1:** Submit to Canvas a link to your fork of this repository, with edits as directed in the code file. There are comments that say `TODO` throughout the files (fewer than in HW4!). You should fix up what those comments say to make the whole application run.
+
+* **For Part 2:** This repository also includes a markdown file with questions. You should answer all of those questions in that file, and commit the answers to the repository. We'll see these within the link to your fork.
+
+* **For Part 2:** Also submit to canvas a file: An image (which you could use software to make, or draw on paper and scan/take a picture of it) demonstrating how data will flow through your application. See below for more instructions. Attach this to your Canvas submission.
+
+
+## Instructions
+
+**Your HW5 has two parts:**
+
+* Completing an almost-complete Flask application to continue practicing the skills you've been learning in class. This is much shorter than HW4.
+
+* Designing a plan and diagram for the application you will write for your final project, which you'll also have time during section to workshop and work on.
+
+### Part 1 (400 points)
+
+Search for all occurrences of `TODO` in the comments in the `.py` files. Fix them as directed to make the application work.
+
+This is a lot less work than your HW4 was, and relies on some of the stuff you've already seen in HW4.
+
+### Part 2 (600 points
+
+Check out the [final project requirements](http://updatelink.com).
+
+The file `final_project_app_qs.md` in this repository contains questions about:
+
+    * How data will flow through your application
+    * How a user could interact with your application
+    * What tables will be in your database and what relationships they will have
+    * Other things!
+
+You should edit that file to answer all of those questions.
+
+**You should also submit a diagram showing how data will basically flow through your application.** You should consider, in your diagram:
+
+* What data does a user enter? In what way?
+* How is the data sent through the application, from the client to a view function? Which view function is it sent to?
+* Is it processed with Python code in any way?
+* Is it saved in any database table?
+* What would keep data from being saved in a database table (think about our songs example -- where you couldn't save songs with the same title)?
+* What queries are made in different view functions?
+* How would a user get from one page in the application to another? A link? Submitting a form and getting redirected?
+
+Part 2 will be graded based on being thoughtful, not necessarily stating things that will absolutely work. We will give you feedback on what you submit for your Part 2 to help you get started on the final project.
---
+++ SI364_HW5.py
@@ -0,0 +1,191 @@
+## SI 364
+## HW5
+
+## Import statements
+import os
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_script import Manager, Shell
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_migrate import Migrate, MigrateCommand
+
+# Configure base directory of app
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Application configurations
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'hardtoguessstringfromsi364(thisisnotsupersecure)'
+## Create a database in postgresql in the code line below, and fill in your app's database URI. It should be of the format: postgresql://localhost/YOUR_DATABASE_NAME
+
+## TODO: Create databaes and change the SQLAlchemy Database URI.
+## Your Postgres database should be your uniqname, plus HW5, e.g. "jczettaHW5" or "maupandeHW5"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/hw5_364"
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# TODO: Add configuration specifications so that email can be sent from this application, like the examples you saw in the textbook and in class. Make sure you've installed the correct library with pip! See textbook.
+# TODO: Make sure that you DO NOT write your actual email password in text!!!!
+# TODO: You will need to use a gmail account to follow the examples in the textbook, and you can create one of those for free, if you want. In THIS application, you should use the username and password from the environment variables, as directed in the textbook. So when WE run your app, we will be using OUR email, not yours.
+
+# Set up Flask debug stuff
+manager = Manager(app)
+db = SQLAlchemy(app) # For database use
+migrate = Migrate(app, db) # For database use/updating
+manager.add_command('db', MigrateCommand) # Add migrate
+# TODO: Run commands to create your migrations folder and get ready to create a first migration, as shown in the textbook and in class.
+
+## Set up Shell context so it's easy to use the shell to debug
+def make_shell_context():
+    return dict(app=app, db=db, Tweet=Tweet, User=User, Hashtag=Hashtag)
+# Add function use to manager
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+# TODO: Write a send_email function here.
+
+#########
+######### Everything above this line is important/useful setup, not problem-solving.
+#########
+#########
+
+##### Set up Models #####
+
+# Association table - Tweets and Hashtags
+tweet_hashtags = db.Table('tweet_hashtags', db.Column('tweet_id', db.Integer, db.ForeignKey('tweets.id')), db.Column('hashtag_id', db.Integer, db.ForeignKey('hashtags.id')))
+
+# Tweet model
+class Tweet(db.Model):
+    __tablename__ = "tweets"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(285))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    hashtags = db.relationship('Hashtag', secondary=tweet_hashtags, backref=db.backref('tweets', lazy='dynamic'),lazy='dynamic')
+
+    def __repr__(self):
+        return "{}, (ID: {})".format(self.text,self.id)
+
+# TODO: Add and run a  migration so that each twitter username also saves an associated email.
+# User model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True) ## -- id (Primary Key)
+    twitter_username = db.Column(db.String(64), unique=True)
+    def __repr__(self):
+        return "{} (ID: {})".format(self.twitter_username,self.id)
+
+# Hashtag model
+class Hashtag(db.Model):
+    __tablename__ = 'hashtags'
+    id = db.Column(db.Integer, primary_key=True) ## -- id (Primary Key)
+    text = db.Column(db.String, unique=True) ## -- text (Unique=True) #represents a single hashtag (like UMSI)
+
+    def __repr__(self):
+        return "{} (ID: {})".format(self.text,self.id)
+
+
+##### Set up Forms #####
+
+# TODO: Add a field in the form for a user to enter their email (the email that goes with the twitter username entered).
+# TODO: Edit the template that renders the form so that email is also asked for!
+class TweetForm(FlaskForm):
+    text = StringField("What is the text of your tweet? Please separate all hashtags with commas in this case. e.g. 'Yay Python #python, #programming, #awesome' ", validators=[Required()])
+    username = StringField("What is your Twitter username?",validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+##### Helper functions
+
+### For database additions / get_or_create functions
+## Write get_or_create functions for each model -- Tweets, Hashtags, and Users.
+## -- Tweets should be identified by their text and user id,(e.g. if there's already a tweet with that text, by that user, then return it; otherwise, create it)
+## -- Users should be identified by their username (e.g. if there's already a user with that username, return it, otherwise; create it)
+## -- Hashtags should be identified by their text (e.g. if there's already a hashtag with that text, return it; otherwise, create it)
+
+# TODO: Edit get_or_create_user (AND get_or_create_tweet -- see below) as necessary to store a user's email as well as their twitter username. The get_or_create_user function should accept an email as input and deal with it appropriately to save it as part of a User row. Each user (from now on) has an email!
+def get_or_create_user(db_session, username):
+    user = db_session.query(User).filter_by(twitter_username=username).first()
+    if user:
+        return user
+    else:
+        user = User(twitter_username=username)
+        db_session.add(user)
+        db_session.commit()
+        return user
+
+def get_or_create_hashtag(db_session, hashtag_given):
+    hashtag = db_session.query(Hashtag).filter_by(text = hashtag_given).first()
+    if hashtag:
+        return hashtag
+    else:
+        hashtag = Hashtag(text=hashtag_given)
+        db_session.add(hashtag)
+        db_session.commit()
+        return hashtag
+
+# TODO: Will need to make changes in this function as well, to address users having emails. See above. What do you need to change to make sure get_or_create_user is invoked correctly, including saving an email?
+def get_or_create_tweet(db_session, input_text, username):
+    tweet = db_session.query(Tweet).filter_by(text=input_text, user_id=get_or_create_user(db_session, username).id).first()
+    if tweet:
+        return tweet
+    else:
+        user = get_or_create_user(db_session, username)
+        tweet = Tweet(text=input_text, user_id=user.id)
+        for text in input_text.split(','):
+            if "#" in text.strip():
+                pos = text.find('#')
+                word = text[pos:].replace("#", '')
+                hashtag = get_or_create_hashtag(db_session, word.strip())
+                tweet.hashtags.append(hashtag)
+        db_session.add(tweet)
+        db_session.commit()
+        return tweet
+
+##### Controllers (view functions) #####
+
+## Error handling routes
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+# TODO: Edit the index route so that, when a tweet is saved by a certain user, that user gets an email. Use the send_email function (just like the one in the textbook) that you defined above.
+# NOTE: You may want to create a test gmail account to try this out so testing it out is not annoying. You can also use other ways of making test emails easy to deal with, as discussed in class!
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    tweets = Tweet.query.all()
+    num_tweets = len(tweets)
+    form = TweetForm()
+    if form.validate_on_submit():
+        if db.session.query(Tweet).filter_by(text=form.text.data, user_id= (get_or_create_user(db.session, form.username.data).id)).first():
+            flash("You've already saved that tweet by this user!")
+        get_or_create_tweet(db.session, form.text.data, form.username.data)
+        return redirect(url_for('see_all_tweets'))
+    return render_template('index.html', form=form,num_tweets=num_tweets)
+
+@app.route('/all_tweets')
+def see_all_tweets():
+    all_tweets = []
+    tweets = Tweet.query.all()
+    for t in tweets:
+        user = User.query.filter_by(id=t.user_id).first()
+        all_tweets.append((t.text, user.twitter_username))
+    return render_template('all_tweets.html', all_tweets=all_tweets)
+
+@app.route('/all_users')
+def see_all_users():
+    all_users = []
+    users = User.query.all()
+    all_tweets = [(Tweet.query.filter_by(user_id=user.id).count(), user.twitter_username) for user in users]
+    return render_template('all_tweets.html', all_tweets=all_tweets)
+
+if __name__ == '__main__':
+    db.create_all()
+    manager.run() # Run with this: python main_app.py runserver
+    # Also provides more tools for debugging
