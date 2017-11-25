d79d17b5c25fc04ce2ed1227c4d56400e5eb4d3d
johnglennvoorhess <voorhessjohn@gmail.com>
2017-11-25 15:44:39 -0500

    initial commit: all done minus half the proposal

--- SI364_HW5.py
+++ SI364_HW5.py
@@ -7,8 +7,10 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager, Shell
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required, Email
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+from threading import Thread
 
 from flask_migrate import Migrate, MigrateCommand
 
@@ -21,22 +23,32 @@ app.debug = True
 app.config['SECRET_KEY'] = 'hardtoguessstringfromsi364(thisisnotsupersecure)'
 ## Create a database in postgresql in the code line below, and fill in your app's database URI. It should be of the format: postgresql://localhost/YOUR_DATABASE_NAME
 
-## TODO: Create database and change the SQLAlchemy Database URI.
+## XTODO: Create database and change the SQLAlchemy Database URI.
 ## Your Postgres database should be your uniqname, plus HW5, e.g. "jczettaHW5" or "maupandeHW5"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/hw5_364"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/voorHW5"
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# TODO: Add configuration specifications so that email can be sent from this application, like the examples you saw in the textbook and in class. Make sure you've installed the correct library with pip! See textbook.
+# XTODO: Add configuration specifications so that email can be sent from this application, like the examples you saw in the textbook and in class. Make sure you've installed the correct library with pip! See textbook.
 # NOTE: Make sure that you DO NOT write your actual email password in text!!!!
 # NOTE: You will need to use a gmail account to follow the examples in the textbook, and you can create one of those for free, if you want. In THIS application, you should use the username and password from the environment variables, as directed in the textbook. So when WE run your app, we will be using OUR email, not yours.
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587 #default
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') 
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_SUBJECT_PREFIX'] = '[HW5]'
+app.config['MAIL_SENDER'] = 'Trustworthy' 
+app.config['ADMIN'] = os.environ.get('ADMIN')
+
 
 # Set up Flask debug stuff
 manager = Manager(app)
 db = SQLAlchemy(app) # For database use
 migrate = Migrate(app, db) # For database use/updating
 manager.add_command('db', MigrateCommand) # Add migrate
-# TODO: Run commands to create your migrations folder and get ready to create a first migration, as shown in the textbook and in class.
+mail = Mail(app)
+# XTODO: Run commands to create your migrations folder and get ready to create a first migration, as shown in the textbook and in class.
 
 ## Set up Shell context so it's easy to use the shell to debug
 def make_shell_context():
@@ -44,8 +56,19 @@ def make_shell_context():
 # Add function use to manager
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
-# TODO: Write a send_email function here. (As shown in examples.)
+# XTODO: Write a send_email function here. (As shown in examples.)
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
+def send_email(to, subject, template): # kwargs = 'keyword arguments', this syntax means to unpack any keyword arguments into the function in the invocation...
+    msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt')
+    msg.html = render_template(template + '.html')
+    thr = Thread(target=send_async_email, args=[app, msg]) # using the async email to make sure the email sending doesn't take up all the "app energy" -- the main thread -- at once
+    thr.start()
+    return thr # The thread being returned
 #########
 ######### Everything above this line is important/useful setup, not problem-solving.
 #########
@@ -67,12 +90,13 @@ class Tweet(db.Model):
     def __repr__(self):
         return "{}, (ID: {})".format(self.text,self.id)
 
-# TODO: Add and run a  migration so that each twitter username also saves an associated email.
+# XTODO: Add and run a  migration so that each twitter username also saves an associated email.
 # User model
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True) ## -- id (Primary Key)
     twitter_username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(128))
     def __repr__(self):
         return "{} (ID: {})".format(self.twitter_username,self.id)
 
@@ -88,11 +112,12 @@ class Hashtag(db.Model):
 
 ##### Set up Forms #####
 
-# TODO: Add a field in the form for a user to enter their email (the email that goes with the twitter username entered).
-# TODO: Edit the template that renders the form so that email is also asked for!
+# XTODO: Add a field in the form for a user to enter their email (the email that goes with the twitter username entered).
+# XTODO: Edit the template that renders the form so that email is also asked for!
 class TweetForm(FlaskForm):
     text = StringField("What is the text of your tweet? Please separate all hashtags with commas in this case. e.g. 'Yay Python #python, #programming, #awesome' ", validators=[Required()])
     username = StringField("What is your Twitter username?",validators=[Required()])
+    email = StringField("What is your email address?", validators=[Required(),Email()])
     submit = SubmitField('Submit')
 
 
@@ -104,13 +129,13 @@ class TweetForm(FlaskForm):
 ## -- Users should be identified by their username (e.g. if there's already a user with that username, return it, otherwise; create it)
 ## -- Hashtags should be identified by their text (e.g. if there's already a hashtag with that text, return it; otherwise, create it)
 
-# TODO: Edit get_or_create_user (AND get_or_create_tweet -- see below) as necessary to store a user's email as well as their twitter username. The get_or_create_user function should accept an email as input and deal with it appropriately to save it as part of a User row. Each user (from now on) has an email! This should be a small change to how the function currently works.
-def get_or_create_user(db_session, username):
+# XTODO: Edit get_or_create_user (AND get_or_create_tweet -- see below) as necessary to store a user's email as well as their twitter username. The get_or_create_user function should accept an email as input and deal with it appropriately to save it as part of a User row. Each user (from now on) has an email! This should be a small change to how the function currently works.
+def get_or_create_user(db_session, username, email):
     user = db_session.query(User).filter_by(twitter_username=username).first()
     if user:
         return user
     else:
-        user = User(twitter_username=username)
+        user = User(twitter_username=username, email=email)
         db_session.add(user)
         db_session.commit()
         return user
@@ -125,13 +150,13 @@ def get_or_create_hashtag(db_session, hashtag_given):
         db_session.commit()
         return hashtag
 
-# TODO: You will need to make changes in this function as well, to address users having emails. See above. What do you need to change to make sure get_or_create_user is *invoked* correctly, including saving an email? Does anything need to change about the input to get_or_create_tweet, and *its* invocations?
-def get_or_create_tweet(db_session, input_text, username):
-    tweet = db_session.query(Tweet).filter_by(text=input_text, user_id=get_or_create_user(db_session, username).id).first()
+# XTODO: You will need to make changes in this function as well, to address users having emails. See above. What do you need to change to make sure get_or_create_user is *invoked* correctly, including saving an email? Does anything need to change about the input to get_or_create_tweet, and *its* invocations?
+def get_or_create_tweet(db_session, input_text, username, email):
+    tweet = db_session.query(Tweet).filter_by(text=input_text, user_id=get_or_create_user(db_session, username, email).id).first()
     if tweet:
         return tweet
     else:
-        user = get_or_create_user(db_session, username)
+        user = get_or_create_user(db_session, username, email)
         tweet = Tweet(text=input_text, user_id=user.id)
         for text in input_text.split(','):
             if "#" in text.strip():
@@ -155,7 +180,7 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-# TODO: Edit the index route so that, when a tweet is saved by a certain user, that user gets an email. Use the send_email function (just like the one in the textbook) that you defined above.
+# XTODO: Edit the index route so that, when a tweet is saved by a certain user, that user gets an email. Use the send_email function (just like the one in the textbook) that you defined above.
 # NOTE: You may want to create a test gmail account to try this out so testing it out is not annoying. You can also use other ways of making test emails easy to deal with, as discussed in class!
 ## This is also very similar to example code.
 @app.route('/', methods=['GET', 'POST'])
@@ -163,10 +188,13 @@ def index():
     tweets = Tweet.query.all()
     num_tweets = len(tweets)
     form = TweetForm()
+    subject = "You saved a tweet"
+    template = "tweet_saved"
     if form.validate_on_submit():
-        if db.session.query(Tweet).filter_by(text=form.text.data, user_id= (get_or_create_user(db.session, form.username.data).id)).first():
+        if db.session.query(Tweet).filter_by(text=form.text.data, user_id= (get_or_create_user(db.session, form.username.data, form.email.data).id)).first():
             flash("You've already saved that tweet by this user!")
-        get_or_create_tweet(db.session, form.text.data, form.username.data)
+        get_or_create_tweet(db.session, form.text.data, form.username.data, form.email.data)
+        send_email(form.email.data, subject, template)
         return redirect(url_for('see_all_tweets'))
     return render_template('index.html', form=form,num_tweets=num_tweets)
 
--- final_project_app_qs.md
+++ final_project_app_qs.md
@@ -3,22 +3,37 @@
 ## Overall
 
 * **What's a one-two sentence description of what your app will do?**
+The app will be a record store so that I can have an excuse to catalog my collection.
 
 ## The Data
 
 * **What data will your app use? From where will you get it? (e.g. scraping a site? what site? -- careful not to run it too much. An API? Which API?)**
+I may use Discogs API through their Python client as I intend to format my csv inventory import tool to mirror their user collection format. https://github.com/discogs/discogs_client  I would probably use this to grab artist info for each product page.
 
 * **What data will a user need to enter into a form?**
+There will be two types of user - an administrator that adds and removes inventory and a shopper.
+The shopper will only need to enter an email and a password.
+The administrator will have the option to add inventory by manually entering all fields - Artist, catalog number, label, album title, release year, condition, notes, price - OR they may choose to do a bulk insert by uploading a properly formatted csv.
 
 * **How many fields will your form have? What's an example of some data user might enter into it?**
+Artist, catalog number, label, album title, release year, condition, notes, price
+Madonna, 9 25157-1, SIRE, Like A Virgin, 1984, VG+, white vinyl dj promo, 75.00
 
 * **After a user enters data into the form, what happens? Does that data help a user search for more data? Does that data get saved in a database? Does that determine what already-saved data the user should see?**
+The data will be commited to a product table in a database. I may choose to direct the user to a page showing all entries by the artist just commited so that they may check to see that there aren't duplicate records. Since conditions vary, each row will represent a unique physical product, so inventory numbers are either 1 or 0. 
 
 * **What models will you have in your application?**
+User, Product, Sales_Order
 
 * **What fields will each model have?**
+User(id, email, password)
+Product(id, artist, catalog number, label, album title, release year, condition, notes, price)
+Sales_Order(id, User.id, line_id, Product.id, Product.price)
 
 * **What uniqueness constraints will there be on each table? (e.g. can't add a song with the same title as an existing song)**
+In the User table, email will be unique as it is inherently unique and id will be unique. 
+In the Product table, nothing will be unique since there may be multiple entries of the same record.
+In Sales_Order, id will be unique
 
 * **What relationships will exist between the tables? What's a 1:many relationship between? What about a many:many relationship?**
 
